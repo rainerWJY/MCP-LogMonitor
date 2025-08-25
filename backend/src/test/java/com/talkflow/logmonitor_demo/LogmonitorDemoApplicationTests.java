d0bc903505018c8a891e0b44a6226132025fc475
rainerWJY <answeropensource@alibabacloud.com>
2025-08-25 10:57:39 +0800

    Enhance README with further details and improved formatting

---
+++ backend/src/test/java/com/talkflow/logmonitor_demo/LogmonitorDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.talkflow.logmonitor_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogmonitorDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
